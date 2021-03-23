a527f4d44e136466a2a3f39896c8d5db23746953
Tanner Robert Pedretti <69763592+VoodooJellyfish@users.noreply.github.com>
2021-03-23 18:55:24 -0400

    Delete employee.js

--- js/employee.js
+++
@@ -1,7 +0,0 @@
-//reading in csv
-d3.csv("tanners_data/cleaned_data.csv").then((importedData) => {
-    Object.entries(importedData).forEach(function([k, v]) {
-        var enrollee_ids = v.enrollee_id
-        console.log(enrollee_ids);
-    });
-});
